ab1bb79aeb2ad5946690107a864f4a77248c4cdd
denitsaradeva <deniiradeva@yahoo.com>
2023-03-09 12:34:59 +0000

    contract fixes

--- src/contracts/pollSystem_contract.py
+++ src/contracts/pollSystem_contract.py
@@ -1,6 +1,5 @@
 from pyteal import *
 
-
 class Poll:
     class Variables:
         title = Bytes("TITLE")
@@ -15,15 +14,15 @@ class Poll:
     
         on_creation = Seq([
             Assert(Txn.application_args.length() == Int(2)),
-            Assert(Txn.note() == Bytes("polling-system:uv7")),
+            Assert(Txn.note() == Bytes("polling-system:uv8")),
             App.globalPut(self.Variables.title, Txn.application_args[0]),
             App.globalPut(self.Variables.options, Txn.application_args[1]),
             App.globalPut(self.Variables.creator, Txn.sender()),
             Approve()
         ])
         
-        is_creator = Txn.sender() == App.globalGet(Bytes("CREATOR"))
-        get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("VOTED"))
+        is_creator = Txn.sender() == App.globalGet(self.Variables.creator)
+        get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
         
         on_closeout = Seq([
             get_vote_of_sender,
@@ -42,11 +41,13 @@ class Poll:
         choice = Txn.application_args[1]
         choice_tally = App.globalGet(choice)
         
+        check_choice = If(choice_tally > Int(0), App.globalPut(choice, choice_tally + Int(1)), App.globalPut(choice, Int(1)))
+        
         on_vote = Seq([
             get_vote_of_sender,
             If(get_vote_of_sender.hasValue(), Return(Int(0))),
-            App.globalPut(choice, choice_tally + Int(1)),
-            App.localPut(Int(0), Bytes("VOTED"), choice),
+            check_choice,
+            App.localPut(Int(0), Bytes("voted"), choice),
             Return(Int(1)),
         ])
         
@@ -56,7 +57,7 @@ class Poll:
             [Txn.on_completion() == OnComplete.UpdateApplication, Return(is_creator)],
             [Txn.on_completion() == OnComplete.CloseOut, on_closeout],
             [Txn.on_completion() == OnComplete.OptIn, on_register],
-            [Txn.application_args[0] == Bytes("vote"), on_vote],
+            [Txn.on_completion() == OnComplete.NoOp, on_vote],
         )
 
         return program
@@ -66,5 +67,3 @@ class Poll:
 
     def clear_program(self):
         return Return(Int(1))
-
-
