aa04ffa42de24a0a35df51a3936103411ff991b1
denitsaradeva <deniiradeva@yahoo.com>
2023-04-27 15:06:21 +0100

    comments added

--- src/contracts/pollSystem_contract.py
+++ src/contracts/pollSystem_contract.py
@@ -1,3 +1,7 @@
+#url: https://github.com/algorand/pyteal/tree/b1ab98396e42cb5fe4ca776e7cf4688329aba71d
+#author: Algorand
+#official PyTeal Documentation
+
 from pyteal import *
 
 class Poll:
@@ -12,6 +16,7 @@ class Poll:
 
     def application_start(self):
     
+        # Updates the global state during the creation of the poll
         on_creation = Seq([
             Assert(Txn.application_args.length() == Int(3)),
             App.globalPut(self.Variables.title, Txn.application_args[0]),
@@ -21,16 +26,24 @@ class Poll:
             Approve()
         ])
         
-        is_creator = Txn.sender() == App.globalGet(self.Variables.creator)
-        get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
-        
         on_register = Return(Int(1))
 
+        # Gets the choice from the application call arguments
         choice = Txn.application_args[1]
+
+        # Gets the value from the global state
         choice_tally = App.globalGet(choice)
+
+        #Checks whether the application call sender is the same as the smart contract's creator
+        is_creator = Txn.sender() == App.globalGet(self.Variables.creator)
+
+        #Gets the vote of the sender
+        get_vote_of_sender = App.localGetEx(Int(0), App.id(), Bytes("voted"))
         
+        # Checks whether the choice already exists, if it does, it increments it; if it does not, it adds it to the global state dictionary
         check_choice = If(choice_tally > Int(0), App.globalPut(choice, choice_tally + Int(1)), App.globalPut(choice, Int(1)))
         
+        #Triggered when a vote is cast and handles the global and local state updates
         on_vote = Seq([
             If((Global.latest_timestamp() > App.globalGet(Bytes("EndTime"))), Return(Int(0))),
             get_vote_of_sender,
@@ -40,6 +53,7 @@ class Poll:
             Return(Int(1)),
         ])
         
+        # Entry point where different function gets invoked depending on the type of application call
         program = Cond(
             [Txn.application_id() == Int(0), on_creation],
             [Txn.on_completion() == OnComplete.DeleteApplication, Return(is_creator)],
